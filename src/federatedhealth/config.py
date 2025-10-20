b046396bd8d109b74933086b7a587380b4a880df
Erik Ylipää <erik.ylipaa@gmail.com>
2025-10-20 12:14:53 +0200

    Added functionality to override training arguments

--- src/federatedhealth/config.py
+++ src/federatedhealth/config.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, fields
-from typing import Optional, Literal, Union
+from typing import Optional, Literal, Union, Dict
 import json
 
 from peft import LoraConfig, TaskType
@@ -36,7 +36,14 @@ class TrainingArgs:
     num_warmup_steps: int = 0
     checkpointing_steps: Optional[Union[Literal['epoch'], int]] = None
     aggregation_epochs: int = 1
-        
+    train_samples: Optional[int] = None  # If given, training will only use this many samples
+    eval_samples: Optional[int] = None  # If steps are given, evaluation will only use these many samples
+
+    def update(self, updates: Dict):
+        for f in fields(self):
+            if f.name in updates:
+                setattr(self, f.name, updates[f.name])
+    
 @dataclass
 class DataConfig:
     training_data: str
@@ -49,42 +56,53 @@ class FHConfig:
     lora_config: LoraConfig
     data_config: DataConfig
     model_path: str
+    
+    
 
-
-def load_config() -> FHConfig:
+def load_config(override_path: Optional[str]=None) -> FHConfig:
     config_path = ensure_config_exists()
     with open(config_path) as f:
         config_dict = json.load(f)
         trainings_args = config_dict['training_args']
         lora_config = config_dict['lora_config']
         data_config = config_dict['data_config']
-                
-        # As a hack, we manually parse the things which aren't basic types at this moment.
-        # There's likely more elegant ways of doing this, but for now 
-        # we're sticking with ones which are secure. This assumes that the task_type 
-        # field in the JSON corresponds to the enum attribute of TaskType
-        lora_config['task_type'] = getattr(TaskType, lora_config['task_type'])
         
-        training_args = TrainingArgs(**trainings_args)
-        lora_config = LoraConfig(**lora_config)
-        data_config = DataConfig(**data_config)
-        model_path = config_dict['model_path']
+        if override_path is not None and os.path.exists(override_path):
+            with open(override_path) as fp:
+                override_config = json.load(fp)
+                override_training_args = override_config.get('training_args')
+                if override_training_args is not None:
+                    trainings_args.update(override_training_args)
+                override_lora_args = override_config.get('lora_config')
+                if override_lora_args is not None:
+                    lora_config.update(override_lora_args)
+            
+            # We did a check to make sure config files exist. This breaks things server-side, where configs do not have to exist.
+            # all_exists = True
+            # for path_attr in fields(data_config):
+            #     path = getattr(data_config, path_attr.name)
+            #     if not os.path.exists(path):
+            #         print(f"Path for data_config.{path_attr.name} ({path}) does not exist!")
+            #         all_exists = False
+            
+            # if not all_exists:
+            #     raise RuntimeError(f"Dataset paths for config file {config_path} are incorrect.")
+            
+            # if not os.path.exists(model_path):
+            #     raise RuntimeError(f"Model path {model_path} from config file {config_path} does not exist.")
+    
+    # As a hack, we manually parse the things which aren't basic types at this moment.
+    # There's likely more elegant ways of doing this, but for now 
+    # we're sticking with ones which are secure. This assumes that the task_type 
+    # field in the JSON corresponds to the enum attribute of TaskType
+    lora_config['task_type'] = getattr(TaskType, lora_config['task_type'])
+    training_args = TrainingArgs(**trainings_args)
+    lora_config = LoraConfig(**lora_config)
+    data_config = DataConfig(**data_config)
+    model_path = config_dict['model_path']
         
-        # all_exists = True
-        # for path_attr in fields(data_config):
-        #     path = getattr(data_config, path_attr.name)
-        #     if not os.path.exists(path):
-        #         print(f"Path for data_config.{path_attr.name} ({path}) does not exist!")
-        #         all_exists = False
-        
-        # if not all_exists:
-        #     raise RuntimeError(f"Dataset paths for config file {config_path} are incorrect.")
-        
-        # if not os.path.exists(model_path):
-        #     raise RuntimeError(f"Model path {model_path} from config file {config_path} does not exist.")
-        
-        config = FHConfig(model_path=model_path, training_args=training_args,
-                          lora_config=lora_config,
-                          data_config=data_config)
-        return config
+    config = FHConfig(model_path=model_path, training_args=training_args,
+                        lora_config=lora_config,
+                        data_config=data_config)
+    return config
     
