7e3e3b50cab7516b1afa923d406fb0d617e17ea7
Erik Ylipää <erik.ylipaa@gmail.com>
2026-01-21 15:57:42 +0100

    Added option to disable tqdm

--- src/federatedhealth/config.py
+++ src/federatedhealth/config.py
@@ -28,6 +28,7 @@ class TrainingArgs:
     optimization_batch_size: int = 32
     per_device_train_batch_size: int = 4
     per_device_eval_batch_size: int = 4
+    disable_tqdm: Optional[bool] = False
     learning_rate: float = 1e-4
     weight_decay: float = 1e-6
     max_train_steps: Optional[int] = None
