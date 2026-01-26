1cc8ca4fb999fc15689816ddbc0e41235e0e2575
Erik Ylipää <erik.ylipaa@gmail.com>
2026-01-26 09:21:55 +0100

    Made the vector storage more structured and added support for stop words and word classification

--- aimplant_demonstrator/calculate_vectors.py
+++ aimplant_demonstrator/calculate_vectors.py
@@ -4,6 +4,7 @@ import json
 import csv
 import os
 from functools import partial
+from typing import Optional
 
 import torch
 from torch.utils.data import DataLoader
@@ -35,6 +36,8 @@ def main():
     parser.add_argument('data',
                         help="Path to text file with data to generate vectors for",
                         type=Path)
+    parser.add_argument('--positive-words', help="If supplied, will be used to tag words as \"positive\", which can be used downstreams", type=Path)
+    parser.add_argument('--stop-list', help="If supplied, will be used as a filter and skip any words in this list", type=Path)
     parser.add_argument('--output_dir',
                         help="Path to directory to save output files",
                         type=Path)
@@ -71,13 +74,25 @@ def main():
     collator_fn = partial(collate_batch, tokenizer=model.tokenizer)
     dataloader = DataLoader(tokenized_dataset, num_workers=args.num_dl_workers, batch_size=args.batch_size, collate_fn=collator_fn)
     word_vectors_file = output_dir / 'vectors.h5'
-    word_sums_file = output_dir / 'vector_sums.sqlite'
+    word_sums_files = {layer_idx: output_dir / f'vector_sums_layer_{layer_idx}.sqlite' for layer_idx in args.layer_states}
+    word_sums_dbs = {layer_idx: SqliteDict(f, autocommit=False) for layer_idx, f in word_sums_files.items()}
 
-    with torch.inference_mode(), h5py.File(word_vectors_file, 'w') as store, SqliteDict(word_sums_file, autocommit=False) as db:
+    stop_list = set()
+    if args.stop_list is not None:
+        with open(args.stop_list) as fp:
+            stop_list = set(w for line in fp for w in line.strip().lower().split())
+    
+    positive_words = set()
+    if args.positive_words is not None:
+        with open(args.positive_words) as fp:
+            positive_words = set(w for line in fp for w in line.strip().lower().split())
+    
+    with torch.inference_mode(), h5py.File(word_vectors_file, 'w') as store:
         model.eval()
-        vector_sums = dict()
-        vector_chunk = []
+        vector_sums = {l: {} for l in args.layer_states}
+        vector_chunks = {l: [] for l in args.layer_states}
         words_chunk = []
+        word_class_chunk = []
         chunk_size = args.chunk_size
 
         for batch in tqdm(dataloader):
@@ -90,34 +105,47 @@ def main():
             
             for example_idx, (words, word_groups) in enumerate(zip(batch["words"], batch["word_token_groups"])):
                 for word_idx, (word, word_group) in enumerate(zip(words, word_groups)):
-                    vector_stack = []
+                    word = word.lower()
+                    if word in stop_list:
+                        continue
+
                     for layer_idx in args.layer_states:
                         layer_states = hidden_states[layer_idx]
                         word_vectors = layer_states[example_idx, word_group]
                         word_vector = word_vectors.mean(dim=0)
-                        vector_stack.append(word_vector)
-                    stacked = torch.stack(vector_stack).to(torch.float16).cpu().numpy()
-                    vector_chunk.append(stacked)
-                    words_chunk.append(word)
-                    if len(vector_chunk) >= chunk_size:
-                        store_data(vector_chunk, words_chunk, store, chunk_size=chunk_size)
-                        vector_chunk.clear()
-                        words_chunk.clear()
-
-                    if word not in vector_sums:
-                        vector_sums[word] = (1, stacked)
-                    else:
-                        prev_count, prev_sum = vector_sums[word]
-                        vector_sums[word] = (prev_count + 1, prev_sum + stacked)
-                    
-                    if len(vector_sums) > chunk_size:
-                        sync_vector_sums(vector_sums, db)
-                        vector_sums.clear()
-
-        store_data(vector_chunk, words_chunk, store, chunk_size=chunk_size)
-        sync_vector_sums(vector_sums, db)
+                        local_word_vector = word_vector.to(torch.float16).cpu().numpy()
+                        vector_chunks[layer_idx].append(local_word_vector)
                         
-        
+                        layer_vector_sums = vector_sums[layer_idx]
+                        if word not in layer_vector_sums:
+                            layer_vector_sums[word] = (1, local_word_vector)
+                        else:
+                            prev_count, prev_sum = layer_vector_sums[word]
+                            layer_vector_sums[word] = (prev_count + 1, prev_sum + local_word_vector)
+                    
+                    positive_word = word in positive_words
+                    word_class_chunk.append(positive_word)
+                    words_chunk.append(word)
+                    
+                    if len(words_chunk) >= chunk_size:
+                        store_data(vector_chunks, words_chunk, word_class_chunk, store, chunk_size=chunk_size)
+                        for v in vector_chunks.values():
+                            v.clear()
+                        words_chunk.clear()
+                        word_class_chunk.clear()
+
+                    for layer_idx, layer_vector_sums in vector_sums.items():
+                        if len(layer_vector_sums) > chunk_size:
+                            db = word_sums_dbs[layer_idx]
+                            sync_vector_sums(layer_vector_sums, db)
+                            layer_vector_sums.clear()
+
+        store_data(vector_chunks, words_chunk, word_class_chunk, store, chunk_size=chunk_size)
+        for layer_idx, layer_vector_sums in vector_sums.items():
+            db = word_sums_dbs[layer_idx]
+            sync_vector_sums(layer_vector_sums, db)
+            layer_vector_sums.clear()
+            db.close()
 
 def sync_vector_sums(vector_sums, db):
     for word, (count, vectors) in vector_sums.items():
@@ -128,24 +156,39 @@ def sync_vector_sums(vector_sums, db):
         db[word] = (count, vectors)
     db.commit()
 
-def store_data(vector_chunk, word_chunk, store, chunk_size=128):
-    stacked_vector_chunk = np.stack(vector_chunk)
-    n_vectors, *vector_shapes = stacked_vector_chunk.shape
-    if 'vectors' not in store:
-        store.create_dataset('vectors', 
-                                data=stacked_vector_chunk, 
-                                chunks=(chunk_size, *vector_shapes), 
-                                maxshape=(None, *vector_shapes),
-                                compression='gzip',
-                                compression_opts=9)
+def store_data(vector_chunk, word_chunk, word_class_chunk, store, chunk_size=128):
+    if 'words' not in store:
         store.create_dataset('words', data=word_chunk, dtype=h5py.string_dtype(), maxshape=(None,))
+        store.create_dataset('word_classes', data=np.array(word_class_chunk), maxshape=(None,))
     else:
-        current_size = store['vectors'].shape[0]
-        new_size = current_size + n_vectors
-        store['vectors'].resize(new_size, axis=0)
-        store['vectors'][current_size:new_size] = stacked_vector_chunk
+        current_size = store['words'].shape[0]
+        new_size = current_size + len(word_chunk)
         store['words'].resize(new_size, axis=0)
         store['words'][current_size:new_size] = word_chunk
+        store['word_classes'].resize(new_size, axis=0)
+        store['word_classes'][current_size:new_size] = np.array(word_class_chunk)
+
+    vectors_group = store.require_group("vectors")
+        
+    for layer_idx, vectors in vector_chunk.items():
+        stacked_vector_chunk = np.stack(vectors)
+        n_vectors, *vector_shapes = stacked_vector_chunk.shape
+        layer_name = f'layer:{layer_idx}'
+        
+        if layer_name not in vectors_group:
+            layer_dataset = vectors_group.create_dataset(layer_name, 
+                                    data=stacked_vector_chunk, 
+                                    chunks=(chunk_size, *vector_shapes), 
+                                    maxshape=(None, *vector_shapes),
+                                    compression='gzip',
+                                    compression_opts=9)
+            layer_dataset.attrs['layer_idx'] = layer_idx
+        else:
+            current_size = vectors_group[layer_name].shape[0]
+            new_size = current_size + n_vectors
+            vectors_group[layer_name].resize(new_size, axis=0)
+            vectors_group[layer_name][current_size:new_size] = stacked_vector_chunk
+            
 
 
                 
