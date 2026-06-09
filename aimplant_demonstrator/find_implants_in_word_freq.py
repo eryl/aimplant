6a9ca16f302f6205999a4b36f5aeb7a63d39764d
Arne Jönsson <arne.jonsson@liu.se>
2026-06-09 13:34:44 +0200

    changed glossary

--- aimplant_demonstrator/find_implants_in_word_freq.py
+++ aimplant_demonstrator/find_implants_in_word_freq.py
@@ -1,8 +1,9 @@
 items = set()
+glossary = '/home/abragam23/fedhealth_data/Glossary_only_known_implants.txt'
 with open('word_freq_summary_085.txt', 'r', encoding='utf-8') as f:
-    with open('glossary.txt', 'r', encoding='utf-8') as g:
+    with open(glossary, 'r', encoding='utf-8') as g:
         glossary = [line.strip() for line in g if line.strip()]
-        print(f"Loaded {len(glossary)} items from glossary.txt")
+        print(f"Loaded {len(glossary)} items from {glossary}")
         for line in f:
             item = line.split(':')[0].strip()
             print(f"Checking item: {item}")
