421fd9ba7c954420c69f919ac216d7da95e70f12
Arne Jönsson <arne.jonsson@liu.se>
2026-06-09 13:41:07 +0200

    update

--- aimplant_demonstrator/find_implants_in_word_freq.py
+++ aimplant_demonstrator/find_implants_in_word_freq.py
@@ -1,9 +1,9 @@
 items = set()
-glossary = '/home/abragam23/fedhealth_data/Glossary_only_known_implants.txt'
+glossary_file = '/home/abragam23/fedhealth_data/Glossary_only_known_implants.txt'
 with open('word_freq_summary_085.txt', 'r', encoding='utf-8') as f:
-    with open(glossary, 'r', encoding='utf-8') as g:
+    with open(glossary_file, 'r', encoding='utf-8') as g:
         glossary = [line.strip() for line in g if line.strip()]
-        print(f"Loaded {len(glossary)} items from {glossary}")
+        print(f"Loaded {len(glossary)} items from {glossary_file}")
         for line in f:
             item = line.split(':')[0].strip()
             if item in glossary:
