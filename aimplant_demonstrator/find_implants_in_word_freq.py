923c756e5a37477d0ae05ce97a94b6ca69b0e734
Arne Jönsson <arne.jonsson@liu.se>
2026-06-09 13:38:41 +0200

    update

--- aimplant_demonstrator/find_implants_in_word_freq.py
+++ aimplant_demonstrator/find_implants_in_word_freq.py
@@ -6,14 +6,14 @@ with open('word_freq_summary_085.txt', 'r', encoding='utf-8') as f:
         print(f"Loaded {len(glossary)} items from {glossary}")
         for line in f:
             item = line.split(':')[0].strip()
-            print(f"Checking item: {item}")
             if item in glossary:
+                print(f"Item in glossary: {item}")
                 items.add(line)
 
 output_file = 'implants_in_word_freq.txt'
 
 with open(output_file, 'w', encoding='utf-8') as f:
     for item in items:
-        f.write(str(item))
+        f.write(str(item) + '\n')
 
 print(f"Wrote {len(items)} items to {output_file}")
